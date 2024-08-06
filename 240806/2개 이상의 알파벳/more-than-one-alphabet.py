5e45007aa7aea65e77db9bcc4bef9df0a9419d52
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-06 14:49:45 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240806/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -3,11 +3,11 @@ def check_different_two_cnt(word):
     cnt = 0
 
     for i in range(len(word)):
-        for j in range(i + 1, len(word) - i):
+        for j in range(i + 1, len(word) - i - 1):
             if word[i] != word[j]:
                 cnt += 1
             
-        if cnt >= 2:
+        if cnt >= 1:
             is_result_different = True
 
     if is_result_different:
