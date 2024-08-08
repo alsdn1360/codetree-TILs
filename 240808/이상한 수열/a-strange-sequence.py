84a3a60ad1427d2c1fa89486c9ac1f6e3a349f90
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-08 16:14:09 +0900

    Title: 이상한 수열, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/이상한 수열/a-strange-sequence.py
+++ 240808/이상한 수열/a-strange-sequence.py
@@ -1,18 +1,16 @@
 N = int(input())
-arr = [0, 1]
+arr = [1]
 
-index_N = 1
 result = 0
 
-def find_index_N(index_N, index):
-    if index == N - 1:
-        return index_N
+def find_index_N(index):
+    if index > N:
+        return arr[N - 1]
+    else:
+        arr.append((arr[(index - 2) // 3]) + arr[index - 1])
 
-    index_N = (arr[(index // 3) + 1]) + arr[index]
-    arr.append(index_N)
+        return find_index_N(index + 1)
 
-    return find_index_N(index_N, index + 1)
-
-result = find_index_N(index_N, 1)
+result = find_index_N(1)
 
 print(result)
