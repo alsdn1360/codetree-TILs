4f421188ce866ff2bcf11433002088daec21135c
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-11 21:42:15 +0900

    Title: 비오는 날, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/비오는 날/rainy-day.py
+++ 240811/비오는 날/rainy-day.py
@@ -11,11 +11,15 @@ for _ in range(n):
     date, day, weather = tuple(input().split())
     forecasts.append(forecast(date, day, weather))
 
-min_idx = 0
-for i in range(1, n):
+first_rain_idx = 0
+for i in range(n):
     if forecasts[i].weather == 'Rain':
-        if int(forecasts[min_idx].date[:4]) < int(forecasts[i].date[:4]):
-            min_idx = i
-            break
+        first_rain_idx = i
+        break
 
-print(f'{forecasts[min_idx].date} {forecasts[min_idx].day} {forecasts[min_idx].weather}')
+for i in range(first_rain_idx + 1, n):
+    if forecasts[i].weather == 'Rain':
+        if int(forecasts[first_rain_idx].date[:4]) > int(forecasts[i].date[:4]):
+            first_rain_idx = i
+
+print(f'{forecasts[first_rain_idx].date} {forecasts[first_rain_idx].day} {forecasts[first_rain_idx].weather}')
