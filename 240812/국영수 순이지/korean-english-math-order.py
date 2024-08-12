384c5a7272c212aa0a56e44007b0f8cbbfc60da3
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-12 14:06:30 +0900

    Title: 국영수 순이지, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240812/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,18 @@
+class Score():
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(input())
+
+students = []
+for _ in range(n):
+    name, kor, eng, math = tuple(input().split())
+    students.append(Score(name, int(kor), int(eng), int(math)))
+
+students.sort(key = lambda x : (x.kor, x.eng, x.math), reverse = True)
+
+for student in students:
+    print(student.name, student.kor, student.eng, student.math)
