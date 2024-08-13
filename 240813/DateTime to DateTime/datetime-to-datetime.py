6d08393695ba75fb42e1a5804585b1d11c3ddfb9
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-13 16:10:17 +0900

    Title: DateTime to DateTime, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240813/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,15 @@
+def cal_elapse_time(day, hour, minute):
+    if hour < 11:
+        return -1
+    elif hour == 11 and minute < 11:
+        return -1
+    else:
+        day -= 11
+        hour -= 11
+        minute -= 11
+
+        return (1440 * day) + (60 * hour) + minute
+
+a, b, c = tuple(map(int, input().split()))
+
+print(cal_elapse_time(a, b, c))
