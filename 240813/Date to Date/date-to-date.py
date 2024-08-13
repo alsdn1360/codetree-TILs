8b8876057e8775d11236025e04bd7979316b3dae
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-13 15:57:28 +0900

    Title: Date to Date, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/Date to Date/date-to-date.py
+++ 240813/Date to Date/date-to-date.py
@@ -2,7 +2,7 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def cal_elapse_day(month, day):
     elapse_days = 0
-    for i in range(1, month + 1):
+    for i in range(1, month):
         elapse_days += num_of_days[i]
     elapse_days += day
 
@@ -10,9 +10,4 @@ def cal_elapse_day(month, day):
 
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
-result = cal_elapse_day(m2, d2) - cal_elapse_day(m1, d1)
-
-if result == 0:
-    print('1')
-else:
-    print(result)
+print(cal_elapse_day(m2, d2) - cal_elapse_day(m1, d1) + 1)
