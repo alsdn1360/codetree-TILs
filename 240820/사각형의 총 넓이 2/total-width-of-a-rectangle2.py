e8db0ec8c73d4ede4ae525680e5bd7daf4523e5e
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-20 13:27:36 +0900

    Title: 사각형의 총 넓이 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240820/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+offset = 100
+area = [
+    [0] * 100
+    for _ in range(100)
+]
+
+for _ in range(N):
+    x1, y1, x2, y2 = tuple(map(int, input().split()))
+
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            area[i][j] = 1
+
+total_width = 0
+
+for i in range(100):
+    for j in range(100):
+        if area[i][j] == 1:
+            total_width += 1
+
+print(total_width)
