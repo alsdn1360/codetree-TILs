b3baa7b0bcb8c41c03684dd4c8c505f353f07552
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-20 13:48:51 +0900

    Title: 색종이의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240820/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,26 @@
+offset = 100
+max_range = 2 * offset
+
+matrix = [
+    [0] * max_range
+    for _ in range(max_range)
+]
+
+N = int(input())
+
+for _ in range(N):
+    x, y = tuple(map(int, input().split()))
+    x, y = x + offset, y + offset
+
+    for x in range(x, x + 8):
+        for y in range(y, y + 8):
+            matrix[x][y] += 1
+
+area = 0
+
+for x in range(max_range):
+    for y in range(max_range):
+        if matrix[x][y] == 1:
+            area += 1
+
+print(area)
