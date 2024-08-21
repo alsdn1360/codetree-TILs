53a595bef3ed32d29b0d8296ad8053f5411dc433
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2024-08-21 17:29:43 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240821/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,33 @@
+N, M = tuple(map(int, input().split()))
+
+A = [0]
+B = [0]
+first_person = []
+
+for _ in range(N):
+    v, t = tuple(map(int, input().split()))
+
+    for _ in range(1, t + 1):
+        A.append(A[-1] + v)
+
+for _ in range(M):
+    v, t = tuple(map(int, input().split()))
+
+    for _ in range(1, t + 1):
+        B.append(B[-1] + v)
+
+for i in range(1, len(A)):
+    if A[i] > B[i]:
+        first_person.append('A')
+    elif A[i] < B[i]:
+        first_person.append('B')
+    else:
+        first_person.append(first_person[-1])
+
+change_first_cnt = 0
+
+for i in range(1, len(first_person)):
+    if first_person[i] != first_person[i - 1]:
+        change_first_cnt += 1
+
+print(change_first_cnt)
